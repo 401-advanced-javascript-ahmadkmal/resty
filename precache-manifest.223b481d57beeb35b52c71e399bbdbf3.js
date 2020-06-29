4862fe909eb6205b1677f57fac23cef57ebe2a74
ahmadkmal <runner@fv-az70.wep5xg2qfpnexcppqgs1oq03ud.bx.internal.cloudapp.net>
2020-06-29 23:38:59 +0000

    Updates

---
+++ precache-manifest.223b481d57beeb35b52c71e399bbdbf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a492069163366bf443dd91f918e4df8d",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "70ee916d9faec8245a9e",
+    "url": "/resty/static/css/main.26024cff.chunk.css"
+  },
+  {
+    "revision": "0828aecb8d1951d6e3e4",
+    "url": "/resty/static/js/2.e37d5b6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.e37d5b6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ee916d9faec8245a9e",
+    "url": "/resty/static/js/main.cba21034.chunk.js"
+  },
+  {
+    "revision": "a53a0f6d519226fb9f59",
+    "url": "/resty/static/js/runtime-main.49bf7f19.js"
+  }
+]);
